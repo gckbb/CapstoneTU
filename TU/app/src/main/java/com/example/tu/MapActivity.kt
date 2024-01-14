d343768e4cb7d3708a59e516b2a56e1a6d58e392
gckbb <gckbb1201@gmail.com>
2024-01-14 18:48:25 +0900

    feat::#1 poi마커

--- TU/app/src/main/java/com/example/tu/MapActivity.kt
+++ TU/app/src/main/java/com/example/tu/MapActivity.kt
@@ -3,9 +3,12 @@ package com.example.tu
 import android.graphics.BitmapFactory
 import android.graphics.PointF
 import android.os.Bundle
+import android.util.Log
 import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.skt.tmap.TMapData
+import com.skt.tmap.TMapData.OnFindAllPOIListener
 import com.skt.tmap.TMapPoint
 import com.skt.tmap.TMapView
 import com.skt.tmap.TMapView.OnClickListenerCallback
@@ -25,6 +28,8 @@ class MapActivity : AppCompatActivity() {
         val container: FrameLayout = findViewById(R.id.tmapViewContainer)
         // TMapView 인스턴스를 생성
         val tMapView = TMapView(this@MapActivity)
+        val tMapData = TMapData()
+        val tMapGps = TMapPoint()
         // TMapView를 FrameLayout에 추가
         container.addView(tMapView)
         // 발급받은 키로 TMapView에 API 키 설정
@@ -57,14 +62,30 @@ class MapActivity : AppCompatActivity() {
                 // 맵 로딩이 완료된 후에 수행할 동작을 구현해주세요
                 // 예: 마커 추가, 경로 표시 등
                 Toast.makeText(this@MapActivity, "MapLoading", Toast.LENGTH_SHORT).show()
-                tMapView.setCenterPoint(37.468954, 126.4544153)
+                tMapView.setCenterPoint(tMapGps.katecLat, tMapGps.katecLon)
                 tMapView.zoomLevel = 10
 
                 val marker = TMapMarkerItem()
                 marker.id = "marker1"
-                marker.setTMapPoint(37.468954, 126.4544153)
-                marker.icon = BitmapFactory.decodeResource(resources, R.drawable.poi_dot)
+                marker.setTMapPoint(tMapGps.katecLat, tMapGps.katecLon)
+                marker.icon = BitmapFactory.decodeResource(resources, com.skt.tmap.R.drawable.point)
                 tMapView.addTMapMarkerItem(marker)
+
+                var strData = "메가"
+                tMapData.findAllPOI(strData,
+                    OnFindAllPOIListener { poiItemList ->
+                        for (item in poiItemList) {
+                            Log.e(
+                                "Poi Item",
+                                "name:" + item.poiName + " address:" + item.poiAddress
+                            )
+                            marker.id = item.poiName
+                            marker.setTMapPoint(TMapPoint())
+                            marker.icon = BitmapFactory.decodeResource(resources, com.skt.tmap.R.drawable.poi)
+                            tMapView.addTMapMarkerItem(marker)
+                        }
+                        tMapView.addTMapPOIItem(poiItemList)
+                    })
 
             }
         })
