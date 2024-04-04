0591189c1dc82d246290f12b75d1d8e8cbe65d1d
gckbb <gckbb1201@gmail.com>
2024-04-04 14:22:33 +0900

    체크리스트

---
+++ app/src/main/java/com/example/kakaotest/CheckList/CheckListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kakaotest.CheckList
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kakaotest.R
+
+class CheckListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_list)
+    }
+}
