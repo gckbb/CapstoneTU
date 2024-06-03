8ca6c4f552fcef7dfc5ed9634b4253691f5aeefd
K_Ho <ysking463@gmail.com>
2024-06-03 11:02:27 +0900

    장소추천(작업중)

---
+++ app/src/main/java/com/example/kakaotest/DataModel/Category.kt
@@ -0,0 +1,34 @@
+package com.example.kakaotest.DataModel
+
+import java.io.Serializable
+
+data class CategoryResponse(
+    val response: CategoryResponseData
+)
+
+data class CategoryResponseData(
+    val header: CategoryHeaderData,
+    val body: CategoryBodyData
+)
+
+data class CategoryHeaderData(
+    val resultCode: String,
+    val resultMsg: String
+)
+
+data class CategoryBodyData(
+    val items: CategoryData,
+    val numOfRows: Int,
+    val pageNo: Int,
+    val totalCount: Int
+)
+
+data class CategoryData(
+    val item: List<Category>
+)
+
+data class Category(
+    val rnum: String, //일련번호
+    val code: String, //코드 : 대,중,소분류코드
+    val name: String //코드명 : 대,중,소분류코드명
+): Serializable
